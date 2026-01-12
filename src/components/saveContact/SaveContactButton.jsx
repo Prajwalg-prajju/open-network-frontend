2d716ca20a042f0a2f0d6e1d7ec98005e1561c24
Prajwal G <prajwalgprajju12@gmail.com>
2026-01-12 15:28:14 +0530

    checking

--- src/components/saveContact/SaveContactButton.jsx
+++ src/components/saveContact/SaveContactButton.jsx
@@ -1,26 +1,15 @@
-import React, { useRef } from "react";
+import React from "react";
 import "./saveContact.css";
 import { saveContact } from "./SaveContactUtils";
 
 export default function SaveContactButton({ user }) {
-  const fileInputRef = useRef();
-
   const handleSave = () => {
-    // Pass the file input to saveContact
-    saveContact(user, fileInputRef.current);
+    saveContact(user);
   };
 
   return (
-    <div className="save-contact-wrapper">
-      <input
-        type="file"
-        ref={fileInputRef}
-        accept="image/*"
-        className="profile-image-input"
-      />
-      <button className="save-contact-btn" onClick={handleSave}>
-        Save Contact
-      </button>
-    </div>
+    <button className="save-contact-btn" onClick={handleSave}>
+      Save Contact
+    </button>
   );
 }
